6d22c7e077dbb92c308d7769beb7b5f6c0caaba3
InvinKh <kokonadcat@gmail.com>
2026-06-27 16:48:17 +0300

    update entrypoint for tests outside

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,4 +7,7 @@ set -euo pipefail
 PG_INSTALL_DIR="${PG_INSTALL_DIR:-/usr/local/pgsql}"
 PG_DATA="${PG_DATA:-/var/lib/postgresql/data}"
 
+# Разрешение подключаться со всех портов ез паролей (для тестов снаружи)
+echo "host all all 0.0.0.0/0 trust" >> "${PG_DATA}/pg_hba.conf"
+
 exec "${PG_INSTALL_DIR}/bin/postgres" -D "${PG_DATA}" "$@"
